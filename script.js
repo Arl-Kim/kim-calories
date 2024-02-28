da60fa140b209e6025321c6b4f1cab73c0b0d1ca
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-02-28 08:43:56 +0300

    Check for exponentials e.g 1e10 in input string

--- script.js
+++ script.js
@@ -9,7 +9,13 @@ const resultOutput = document.getElementById("res_output");
 let isError = false;
 
 function cleanInputString(str) {
-    const regex = /[+-\s]/g; // Checks for +, - and white space in String. g is flag (global).
+    const regex = /[+-\s]/g; // Checks for +, - and white space in String. g is flag (global)(all matches).
     
     return str.replace(regex, ""); //Replaces regex values with empty String in str and returns it.
+}
+
+function isInvalidInput(str){
+    const regex = /\d+e\d+/i; //i is flag (insensitive). The + modifier in a regex allows you to match a pattern that occurs one or more times. \d is shorthand for any digit.
+
+    return str.match(regex); //return an array of match results – containing the first match.
 }
