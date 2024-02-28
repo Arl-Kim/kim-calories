9f8098abf207ca46655c115edd27a68aae7aaaf5
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-02-28 07:04:49 +0300

    Declare and initialize variables

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const calorieCounter = document.getElementById("calo_counter");
+const userBudget = document.getElementById("budget");
+const userEntryChoice = document.getElementById("entry_dropdown");
+const addChoiceButton = document.getElementById("add_entry");
+const clearCounterButton = document.getElementById("clear");
+const resultOutput = document.getElementById("res_output");
+let isError = false;
