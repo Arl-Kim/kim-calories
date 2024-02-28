1d36cd6a21674494350d46952e633c30be6ca7ec
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-02-28 07:56:18 +0300

    Clean user input from string to number

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ const addChoiceButton = document.getElementById("add_entry");
 const clearCounterButton = document.getElementById("clear");
 const resultOutput = document.getElementById("res_output");
 let isError = false;
+
+function cleanInputString(str) {
+    const strArray = str.split('');
+    const cleanStrArray = [];
+  
+    for (let i = 0; i < strArray.length; i++) {
+      if (!["+", "-", " "].includes(strArray[i])) {
+        cleanStrArray.push(strArray[i])
+      }
+    }
+  }
