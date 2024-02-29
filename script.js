a4cf446f3d0611a74d8b1b73611c13749ad6812b
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-02-29 09:27:24 +0300

    Define addEntry() function

--- script.js
+++ script.js
@@ -19,3 +19,12 @@ function isInvalidInput(str){
 
     return str.match(regex); //return an array of match results – containing the first match.
 }
+
+function addEntry(){
+    const targetId = "#" + userEntryChoice.value;
+    const targetInputContainer = document.querySelector(targetId + " .input-container"); 
+    //consider using template literal `${targetId} .input-container`, variable targetId won't be required. Check next line.
+    //const targetInputContainer = document.querySelector(`#${userEntryChoice.value} .input-container`);
+
+    const entryNumber = targetInputContainer.querySelectorAll("input[type='text']").length;
+}
