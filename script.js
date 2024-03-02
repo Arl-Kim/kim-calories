22625d1c810a7491c58121cc11fb94816d254756
Allan Kimutai Tum <allankimutaitum@gmail.com>
2024-03-02 07:44:01 +0300

    Change implementation to use template literals

--- script.js
+++ script.js
@@ -21,11 +21,7 @@ function isInvalidInput(str){
 }
 
 function addEntry(){
-    const targetId = "#" + userEntryChoice.value;
-    const targetInputContainer = document.querySelector(targetId + " .input-container"); 
-    //consider using template literal `${targetId} .input-container`, variable targetId won't be required. Check next line.
-    //const targetInputContainer = document.querySelector(`#${userEntryChoice.value} .input-container`);
-
+    const targetInputContainer = document.querySelector(`#${userEntryChoice.value} .input-container`); 
     const entryNumber = targetInputContainer.querySelectorAll("input[type='text']").length;
 
     //Dynamic HTML string
